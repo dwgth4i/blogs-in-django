ace8eef82aa6d503aeabb7f3eb1440da094058cb
Trinh Thai Duong <91010847+dwgth4i@users.noreply.github.com>
2024-06-02 23:42:25 +0700

    Update urls.py

--- mysite/faq/urls.py
+++ mysite/faq/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('', views.post_list, name='post_list'),
     path('<int:year>/<int:month>/<int:day>/<slug:slug>/', views.post_detail, name='post_detail'),
     path('category/<slug:category_slug>/', views.category_list, name='category_list'),
-    path("about/", views.about, name="about")
+    # path("about/", views.about, name="about")
 ]
