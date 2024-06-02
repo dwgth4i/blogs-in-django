87d3a7029c2945c046a36c6648606f04e041a3e2
Trinh Thai Duong <91010847+dwgth4i@users.noreply.github.com>
2024-06-02 23:42:55 +0700

    Update views.py

--- mysite/faq/views.py
+++ mysite/faq/views.py
@@ -1,13 +1,6 @@
-from django.shortcuts import render
-from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
-from .models import Post, Category
-# Create your views here.
-def index(request):
-    return render(request,'faq/index.html')
-
-def about(request):
-    return render(request, 'faq/about.html')
+from .models import Post, Category, Comment
+from .forms import CommentForm
 
 def post_list(request):
     posts = Post.objects.filter(status='published').order_by('-publish')
@@ -15,7 +8,18 @@ def post_list(request):
 
 def post_detail(request, year, month, day, slug):
     post = get_object_or_404(Post, slug=slug, status='published', publish__year=year, publish__month=month, publish__day=day)
-    return render(request, 'faq/post_detail.html', {'post': post})
+    comments = post.comments.filter(active=True)
+
+    if request.method == 'POST':
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            new_comment = comment_form.save(commit=False)
+            new_comment.post = post
+            new_comment.save()
+    else:
+        comment_form = CommentForm()
+
+    return render(request, 'faq/post_detail.html', {'post': post, 'comments': comments, 'comment_form': comment_form})
 
 def category_list(request, category_slug):
     category = get_object_or_404(Category, slug=category_slug)
