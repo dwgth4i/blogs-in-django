f6df6b32f4358465c9d6161cbb405c89dcbe3ba7
V1ncendzo <anh.nh214945@gmail.com>
2024-06-10 10:47:57 +0700

    phu fix

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
-
+admin.site.site_header = "FAQ Admin"
 urlpatterns = [
     path("", include("faq.urls")),
     path("admin/", admin.site.urls),
