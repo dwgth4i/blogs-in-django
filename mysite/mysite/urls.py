c0fa9643632f2310819d829e4af42816584e7b6e
LamPhuss <119296534+LamPhuss@users.noreply.github.com>
2024-06-10 19:08:31 +0700

    Add files via upload

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -16,10 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
-admin.site.site_header = "FAQ Admin"
-admin.site.index_title = "FAQ Admin"
+
+admin.site.site_header = "FAQ Administration"
 
 urlpatterns = [
     path("", include("faq.urls")),
-    path("admin/", admin.site.urls),
+    path("Y29jb25jYWMK/", admin.site.urls),
 ]
